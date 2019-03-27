31a45bef1ae2c9468bde6b383690b955b2dd51db
kalle hålldén <kallehalldeen@gmail.com>
2019-03-27 10:39:51 +0100

    Tabbar implemented

--- netflix_clone/lib/main.dart
+++ netflix_clone/lib/main.dart
@@ -1,63 +1,51 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(Netflix());
 
-class MyApp extends StatelessWidget {
+class Netflix extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primaryColor: Colors.black,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: NetflixTabBar()
     );
   }
 }
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+class NetflixTabBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
+    return DefaultTabController(
+        length: 4,
+        child: Theme(
+          data: ThemeData(
+            brightness: Brightness.dark
+          ),
+          child: Scaffold(
+          bottomNavigationBar: TabBar(
+            tabs: [
+                Tab(icon: Icon(Icons.home,), text: "Home",),
+                Tab(icon: Icon(Icons.search), text: "Search"),
+                Tab(icon: Icon(Icons.file_download), text: "Downloads"),
+                Tab(icon: Icon(Icons.list), text: "More"),
+              ],
+            unselectedLabelColor: Color(0xff999999),
+            labelColor: Colors.white,
+            indicatorColor: Colors.transparent
+          ),
+          body: TabBarView(
+            children: [
+              Center( child: Text("Page 1"), ),
+              Center( child: Text("Page 2")),
+              Center( child: Text("Page 3"),),
+              Center( child: Text("Page 4")),
+            ],
+          ),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+        )
+      );
   }
 }
