25af9dd7abbd90a317b0f37ffdb7036dfb87cc02
kalle hålldén <kallehalldeen@gmail.com>
2019-03-27 15:51:44 +0100

    Available now implemented on home page

--- netflix_clone/lib/homepage.dart
+++ netflix_clone/lib/homepage.dart
@@ -109,11 +109,68 @@ class HomeView extends StatelessWidget {
                   ),
                   makePopularWidget("Popular on Netflix"),
                   makePopularWidget("Trending Now"),
-                  makeContinueWatching("Continue Watching for Kalle")
+                  makeContinueWatching("Continue Watching for Kalle"),
+                  bannerMovie()
           ],
       ),
         )
     ),
+    );
+  }
+
+  Widget bannerMovie() {
+    return new Container(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.only(left: 10),
+            child: Text('Avalable Now', style: topMenuStyle,),          
+            ),
+          Container(child: Image(
+            image: new AssetImage("lib/assets/birdboxBanner.jpg"),
+          ),),
+          Container(
+            padding: EdgeInsets.only(top: 4, bottom: 4),
+            color: Colors.black,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                FlatButton(
+                  child: Container(
+                    padding: EdgeInsets.only(top: 8, bottom: 8),
+                    width: 140,
+                    child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Icon(Icons.play_arrow, color: Colors.black,),
+                      Text('Play', style: TextStyle(color: Colors.black),)
+                    ],
+                  ),
+                  ),
+                  color: Colors.white,
+                  onPressed: () {},
+                ),
+                FlatButton(
+                  child: Container(
+                    padding: EdgeInsets.only(top: 8, bottom: 8),
+                    width: 140,
+                    child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Icon(Icons.add, color: Colors.white,),
+                      Text('My List', style: TextStyle(color: Colors.white),)
+                    ],
+                  ),
+                  ),
+                  color: Color(0xff4f4f4f),
+                  onPressed: () {},
+                )
+              ],
+            ),
+          )
+        ],
+      ),
     );
   }
 
